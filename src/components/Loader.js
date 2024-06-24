739744c5aa0f911f5c74d2e7558dd336626d1940
PabloMunozVillegas <p.munoz@opti-system.com>
2024-06-24 18:16:27 -0400

    cambios

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 z-50">
-            <div className="loader border-t-4 border-b-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+            <div className="loader border-t-4 border-b-4 border-green-500 rounded-full w-16 h-16 animate-spin"></div>
         </div>
     );
 };
